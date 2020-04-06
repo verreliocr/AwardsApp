f45f99196b8ec961cfd3b0a4080c3bb821a05e9b
Verrelio C. Rizky <verrelioragna@gmail.com>
2020-04-06 12:54:15 +0700

    Create base for login page

---
+++ Awards App/Core/Features/Login/LoginInterfaces.swift
@@ -0,0 +1,9 @@
+//
+//  LoginInterfaces.swift
+//  Awards App
+//
+//  Created by Verrelio C. Rizky on 06/04/20.
+//  Copyright © 2020 Verrelio C. Rizky. All rights reserved.
+//
+
+import Foundation
---
+++ Awards App/Core/Features/Login/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Awards App
+//
+//  Created by Verrelio C. Rizky on 06/04/20.
+//  Copyright © 2020 Verrelio C. Rizky. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
